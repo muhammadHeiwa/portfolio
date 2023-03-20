d2237890d05a831da68845096d540004ff05a8cf
heiwaalkahfi <heiwa.alkahfi@kinersia.com>
2023-03-20 10:46:39 +0700

    delete log 2

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,9 +6,6 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
 	const computer = useGLTF('./desktop_pc/scene.gltf');
-	if (isMobile) {
-		console.log(isMobile);
-	}
 
 	return (
 		<mesh>
