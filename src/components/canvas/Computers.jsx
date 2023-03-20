d20927c14b000e167d3318b7148d20cd83cc312d
heiwaalkahfi <heiwa.alkahfi@kinersia.com>
2023-03-20 10:41:41 +0700

    fixing mobile 3

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,6 +6,9 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
 	const computer = useGLTF('./desktop_pc/scene.gltf');
+	if (isMobile) {
+		console.log(isMobile);
+	}
 
 	return (
 		<mesh>
